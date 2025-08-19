aa74f4370786bc27302eb0f514f92429f1f48082
Khair69 <khair04@outlook.com>
2025-08-19 16:43:08 +0300

    part 3

---
+++ dontnet-blazor-wasm-part3/Pages/CarInfo.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace dontnet_blazor_wasm_part3.Pages
+{
+    public partial class CarInfo
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Parameter]
+        public string Name { get; set; }
+    }
+}
